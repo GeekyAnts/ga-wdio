508c7957640531446cf84abd04552ee330acb241
Faiz A Farooqui <faizz.af@gmail.com>
2019-04-29 18:17:49 +0530

    fixes: added npmrc

---
+++ stubs/npmrc.js
@@ -0,0 +1,14 @@
+let npmRC = {};
+
+npmRC.filename = '.npmrc';
+
+npmRC.content = `# -----------------------------------------
+#  Settings for ChromeDriver / GeckoDriver
+# Note: Please do not change these values!
+# -----------------------------------------
+chromedriver_version=LATEST
+
+geckodriver_version=LATEST
+`;
+
+module.exports = npmRC;
