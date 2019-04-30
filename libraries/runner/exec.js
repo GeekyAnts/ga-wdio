c3a06ca1e86d513d57d054e14ad188197e97f4a2
Faiz A Farooqui <faizz.af@gmail.com>
2019-04-30 16:46:52 +0530

    fixes: spawn immediately

--- libraries/runner/exec.js
+++ libraries/runner/exec.js
@@ -5,7 +5,7 @@ let executor = {};
 
 executor.init = async (_conf) => {
 	return new Promise(async (resolve) => {
-		const _spawn = await spawn(wdio(), [_conf]);
+		const _spawn = spawn(wdio(), [_conf]);
 
 		_spawn.stdout.on("data", (_data) => _data.toString().trim() !== "" ? console.log(_data.toString()) : "");
 		_spawn.stderr.on("data", (_data) => _data.toString().trim() !== "" ? console.log(_data.toString()) : "");
