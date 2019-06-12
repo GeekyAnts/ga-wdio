b79c9c9855e416b79146c45a4ccd42998afc752d
Faiz A Farooqui <faizz.af@gmail.com>
2019-06-12 15:10:46 +0530

    fixes: error for ERPROTO

--- libraries/runner/api/sync.js
+++ libraries/runner/api/sync.js
@@ -15,29 +15,33 @@ sync.start = async (_app) => {
 			const response = await request(options);
 			const _expected = await expected.init(_api.expected, response);
 
-			terminal.heading(`API\t`.yellow.bold + `${_api.name || 'No Name'}` + ` (${options.method}: ${options.uri})`.white, true);
+			terminal.heading(`API\t`.yellow.bold + `${_api.name || "No Name"}` + ` (${options.method}: ${options.uri})`.white, true);
 			terminal.heading(`Status`.bold.cyan, true);
 
-			terminal.columns('Code'.boldWhite, (response.status >= 400) ? `${response.status}`.red : `${response.status}`.green);
-			terminal.columns('Message'.boldWhite, (response.status >= 400) ? `${response.statusText}`.red : `${response.statusText}`.green);
+			terminal.columns("Code".boldWhite, (response.status >= 400) ? `${response.status}`.red : `${response.status}`.green);
+			terminal.columns("Message".boldWhite, (response.status >= 400) ? `${response.statusText}`.red : `${response.statusText}`.green);
 
 			if (_expected["headers"] && _expected["headers"].length > 0) {
 				terminal.heading(`Headers`.bold.cyan, true);
 
-				_expected["headers"].map(_value => terminal.columns(`${_value['key']}`.boldWhite, `${_value['value']}`.red));
+				_expected["headers"].map(_value => terminal.columns(`${_value["key"]}`.boldWhite, `${_value["value"]}`.red));
 			}
 
 			if (_expected["body"] && _expected["body"].length > 0) {
 				terminal.heading(`Body`.bold.cyan, true);
 
-				_expected["body"].map(_value => terminal.columns(`${_value['key']}`.boldWhite, `${_value['value']}`.red));
+				_expected["body"].map(_value => terminal.columns(`${_value["key"]}`.boldWhite, `${_value["value"]}`.red));
 			}
 
-			terminal.drawLine();
-
 		} catch (e) {
-			console.log(`>> ${_api.name} [Response :: Error]:`.red, e.message);
+			terminal.heading(`API\t`.yellow.bold + `${_api.name || "No Name"}` + ` (${options.method}: ${options.uri})`.white, true);
+			terminal.heading(`Status`.bold.cyan, true);
+
+			terminal.columns("Code".boldWhite, `${e.code}`.red);
+			terminal.columns("Message".boldWhite, `${e.message}`.red);
 		}
+
+		terminal.drawLine();
 	}
 };
 
